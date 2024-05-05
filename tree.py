665d2702e0818bf3408fe6c7e81e71e17d730468
陈其淼 <16174103@sdtbu.edu.cn>
2024-05-05 15:33:24 +0800

    add 二叉树的三种遍历【非递归写法】

--- tree.py
+++ tree.py
@@ -25,6 +25,27 @@ def preorderTraversal(root: TreeNode):
     return ret
 
 
+def preorderTraversal_non_recur(root: TreeNode):
+    """
+    二叉树的先序遍历（非递归）
+    :param root:
+    :return:
+    """
+    ret = []
+    if root is not None:
+        stack = []
+        stack.append(root)
+        while stack:
+            root = stack.pop()
+            ret.append(root.val)
+            # 因为栈的特性（后进先出）所以要先判断右子树
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                stack.append(root.left)
+    return ret
+
+
 def inorderTraversal(root: TreeNode):
     """
     二叉树的中序遍历（左根右）
@@ -44,6 +65,27 @@ def inorderTraversal(root: TreeNode):
     return ret
 
 
+def inorderTraversal_non_recur(root: TreeNode):
+    """
+    二叉树的中序遍历（非递归）
+    :param root:
+    :return:
+    """
+    ret = []
+    stack = []
+    if root is not None:
+        while stack or root is not None:
+            if root is not None:
+                stack.append(root)
+                # 一直找到最左边的孩子
+                root = root.left
+            else:
+                root = stack.pop()
+                ret.append(root.val)
+                root = root.right
+    return ret
+
+
 def postorderTraversal(root: TreeNode):
     """
     二叉树的后序遍历（左右根）
@@ -60,4 +102,29 @@ def postorderTraversal(root: TreeNode):
         _postorderTraversal(root.right)
         ret.append(root.val)
     _postorderTraversal(root)
+    return ret
+
+
+def postorderTraversal_non_recur(root: TreeNode):
+    """
+    二叉树的后序遍历（非递归）
+    :param root:
+    :return:
+    """
+    ret = []
+    if root is not None:
+        stack1 = []
+        stack2 = []
+        stack1.append(root)
+        while stack1:
+            # 遍历顺序变为：根右左
+            root = stack1.pop()
+            stack2.append(root)
+            if root.left:
+                stack1.append(root.left)
+            if root.right:
+                stack1.append(root.right)
+        # 对所有节点做一个逆序处理就变成了左右根
+        while stack2:
+            ret.append(stack2.pop().val)
     return ret
