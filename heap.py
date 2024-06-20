c1c4ce9a144c177f82d3f5fda8ce09d49e5d665c
陈其淼 <16174103@sdtbu.edu.cn>
2024-06-21 00:32:36 +0800

    update heap

--- heap.py
+++ heap.py
@@ -1,22 +1,14 @@
-"""
-堆结构：是一种特殊的完全二叉树
-
-完全二叉树和数组前缀范围的对应：
-1. i 位置的父亲节点是：(i - 1) / 2
-2. i 位置的左孩子是：(i * 2) + 1
-3. i 位置的右孩子是：(i * 2) + 2
-
-堆的定义（大顶堆）：这个结构中任何一个子结构的最大值都在其顶部
-"""
-
-
 class Heap:
 
-    def __init__(self, n):
-        self.cap = n
+    def __init__(self, n, cmp=lambda a, b: a > b):
+        """大顶堆定义：maxHeap = Heap(n, cmp=lambda a, b: a > b)
+        小顶堆定义：minHeap = Heap(n, cmp=lambda a, b: a < b)
+        """
+        self._cap = n
         self._data = [None for _ in range(n)]
         # 当前堆的大小
         self._size = 0
+        self._cmp = cmp
 
     def heappush(self, value):
         """
@@ -24,12 +16,12 @@ class Heap:
         :param value:
         :return:
         """
-        if self._size >= self.cap:
+        if self._size >= self._cap:
             return
         self._data[self._size], i = value, self._size
         self._size += 1
-        # 当前节点的值是否大于其父亲节点的值
-        while i > 0 and self._data[i] > self._data[(i - 1) // 2]:
+        # 当前节点的值是否大于/小于其父亲节点的值
+        while i > 0 and self._cmp(self._data[i], self._data[(i - 1) // 2]):
             self._data[i], self._data[(i - 1) // 2] = self._data[(i - 1) // 2], self._data[i]
             i = (i - 1) // 2
 
@@ -43,14 +35,14 @@ class Heap:
         l = 2 * i + 1
         # 有左孩子
         while l < self._size:
-            # 取左右孩子最大值的下标
-            best = l + 1 if l + 1 < self._size and self._data[l + 1] > self._data[l] else l
-            # 当前节点与左右孩子的最大值作比较，然后取其最大值的下标
-            best = best if self._data[best] > self._data[i] else i
-            # 如果当前节点比其左右孩子的值都要大，那么符合堆的定义，无需继续往下调整
+            # 取左右孩子最大值/最小值的下标
+            best = l + 1 if l + 1 < self._size and self._cmp(self._data[l + 1], self._data[l]) else l
+            # 当前节点与左右孩子的最大值/最小值作比较，然后取其最大值/最小值的下标
+            best = best if self._cmp(self._data[best], self._data[i]) else i
+            # 如果当前节点比其左右孩子的值都要大/小，那么符合堆的定义，无需继续往下调整
             if best == i:
                 break
-            # 当前节点与左右孩子的最大值交换位置，然后继续往下调整
+            # 当前节点与左右孩子的最大值/最小值交换位置，然后继续往下调整
             self._data[best], self._data[i] = self._data[i], self._data[best]
             i = best
             l = i * 2 + 1
