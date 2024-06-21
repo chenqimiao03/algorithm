00c7d73432fd6c4b6e411f548b85337f243bcc11
陈其淼 <16174103@sdtbu.edu.cn>
2024-06-21 21:55:47 +0800

    update

--- leetcode/136. 只出现一次的数字.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    """
-    验证链接：https://leetcode.cn/problems/single-number/
-    """
-    def singleNumber(self, nums: List[int]) -> int:
-        eor = 0
-        for num in nums:
-            eor ^= num
-        return eor
