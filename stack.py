154c2d358fbff413ed964ec4bd00653db7c452b0
陈其淼 <16174103@sdtbu.edu.cn>
2024-06-26 00:40:18 +0800

    update stack

--- stack.py
+++ stack.py
@@ -36,6 +36,64 @@ class ArrayStack:
 
     def __str__(self):
         return str(self._vals)
+    
+    def sort(self):
+        """时间复杂度：O(N^2)
+        """
+        def _sort(stack):
+            d = deep(stack)
+            while d > 0:
+                maxValue = MAX(stack, d)
+                k = times(stack, d, maxValue)
+                down(stack, d, maxValue, k)
+                d -= k
+            
+        def deep(stack):
+            """利用递归求栈的深度
+            """
+            if len(stack) == 0:
+                return 0
+            top = stack.pop()
+            ret = deep(stack) + 1
+            stack.append(top)
+            return ret
+
+        def MAX(stack, deep):
+            """利用递归查找栈中的最大值
+            """
+            if deep == 0:
+                return -2 ** 31
+            num = stack.pop()
+            ret = max(num, MAX(stack, deep - 1))
+            stack.append(num)
+            return ret
+        
+        def times(stack, d, maxValue):
+            """利用递归统计栈中最大值出现的次数
+            """
+            ret = 0
+            if d == 0:
+                return ret
+            num = stack.pop()
+            ret += 1 if num == maxValue else 0
+            ret += times(stack, d - 1, maxValue)
+            stack.append(num)
+            return ret
+        
+        def down(stack, d, maxValue, k):
+            """利用递归将栈中的最大值下沉到栈底
+            """
+            if d == 0:
+                while k:
+                    stack.append(maxValue)
+                    k -= 1
+                return
+            num = stack.pop()
+            down(stack, d - 1, maxValue, k)
+            if num != maxValue:
+                stack.append(num)
+            return
+        _sort(self._vals)
 
 
 class LinkedStack:
